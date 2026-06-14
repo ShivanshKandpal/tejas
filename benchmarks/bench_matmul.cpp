923bcc23b8631445ae10cc64a6462a303b7bb82d
ShivanshKandpal <shivansh.kandpal.cse22@itbhu.ac.in>
2026-06-14 13:35:38 +0530

    refactor: update bench_matmul to use TensorPtr

--- benchmarks/bench_matmul.cpp
+++ benchmarks/bench_matmul.cpp
@@ -1,20 +1,23 @@
 #include<iostream>
 #include "tensor.h"
 #include<chrono>
+#include<memory>
 int main(){
     std::cout<<"size  |"<<"  naive  |"<<"  tiled  |"<<"  speedup  \n";
     for(int sz = 64;sz<=1024;sz*=2){
         std::vector<float> a_data,b_data;
         for(int i = 0;i<sz*sz;i++){a_data.push_back(rand()%10); b_data.push_back(rand()%10);};
-        Tensor a({sz,sz}, a_data);
-        Tensor b({sz,sz}, b_data);
+        TensorPtr a = std::make_shared<Tensor>(std::vector<int> {sz,sz});
+        a->data = a_data;
+        TensorPtr b = std::make_shared<Tensor>(std::vector<int> {sz,sz});
+        b->data = b_data;
         auto startnaive = std::chrono::high_resolution_clock::now();
-        Tensor c = matmul(a, b);
+        TensorPtr c = matmul_raw(a, b);
         auto endnaive = std::chrono::high_resolution_clock::now();
         double msnaive = std::chrono::duration<double, std::milli>(endnaive-startnaive).count();
         
         auto starttiled = std::chrono::high_resolution_clock::now();
-        Tensor d = matmul_tiled(a, b);
+        TensorPtr d = matmul_tiled_raw(a, b);
         auto endtiled = std::chrono::high_resolution_clock::now();
         
         double mstiled = std::chrono::duration<double,std::milli>(endtiled - starttiled).count();
